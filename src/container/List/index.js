be582f206528cc461086ab93eb7b40f377656ef5
lyxcool <lyxverycool@163.com>
2020-04-24 16:46:21 +0800

    升级版本

--- src/container/List/index.js
+++ src/container/List/index.js
@@ -4,6 +4,7 @@ import './style.less'
 
 export default () => (
   <div className="list">
+    <img src={require('../../images/avatar.jpg')} alt="" />
     <LyxHeader lang="zh_CN" title="测试" />
   </div>
 )

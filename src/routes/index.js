842aeec2cc5b07b2ff13f7b3ebb402a5159ddcbd
lyxcool <lyxverycool@163.com>
2020-04-26 11:24:20 +0800

    修改默认样式

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import routes from './router'
+import '../less/normal.less'
 
 class App extends Component {
   constructor(props) {

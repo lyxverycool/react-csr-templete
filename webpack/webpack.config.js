070b0e72e59daf21088b6da8d5eed17455bb19a5
cool.li <liang@vpgame.cn>
2020-06-30 11:49:35 +0800

    'test_action'

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,3 +1,7 @@
 const { webpackConfig } = require('lyxcool-webpack')
 
+// if (process.env.DEV_SERVER) {
+//   webpackConfig.devServer.disableHostCheck = true
+// }
+
 module.exports = webpackConfig

43a8bb9dddc437523a979d06fa7ddb9be2cd3dbb
cool.li <liang@vpgame.cn>
2020-07-21 11:24:40 +0800

    '注释webpack'

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,6 @@
 import loadable from '@loadable/component'
 
+//跟webpack开发者确认webpack不支持变量name.perhaps feature
 const loadableCom = name => loadable(() => import(/* webpackChunkName: "[request]"*/`../container/${name}/index`))
 
 export default [

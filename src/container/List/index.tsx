89d95c00b6df5fc0043ce5e9accfaa2f77228ce9
cool.li <liang@vpgame.cn>
2020-09-25 11:01:46 +0800

    '配置修改'

--- src/container/List/index.tsx
+++ src/container/List/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { LyxHeader } from 'lyxcool-test'
-import useTitle from '@/ScreenSize'
+import useTitle from '~/component/UseTitle'
 import './style.less'
 
 export default () => {
   useTitle('测试')
   return (
     <div className="list">
-      22
       <img src={require('../../images/avatar.jpg')} alt="" />
       <LyxHeader lang="zh_CN" title="测试" />
     </div>
